a91193b237db51b2ce3d51a65c690c27da2498b5
Philipp Fromme <philippfromme@outlook.com>
2018-08-15 12:48:24 +0200

    Boilerplate

---
+++ example/index.js
@@ -0,0 +1,25 @@
+import bpmnlint from '../lib';
+
+import BpmnJS from 'bpmn-js/lib/Modeler';
+
+import exampleXML from './resources/example.bpmn';
+
+var viewer = new BpmnJS({
+  container: '#canvas',
+  additionalModules: [
+    bpmnlint
+  ],
+  keyboard: {
+    bindTo: document
+  }
+});
+
+viewer.importXML(exampleXML, function(err) {
+  if (!err) {
+    viewer.get('canvas').zoom('fit-viewport');
+  } else {
+    console.log('something went wrong:', err);
+  }
+});
+
+window.viewer = viewer;
