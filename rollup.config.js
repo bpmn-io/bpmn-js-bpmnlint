d6d008e658df16f1e0a7c273d38832c332a31b96
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-08 21:04:11 +0200

    feat(project): provide pre-packed bundles
    * bundle library using rollup and provide { cjs, es } variants
    * distribute bundled artifacts in <dist> folder
    
    Related to #5

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import pkg from './package.json';
+
+import json from 'rollup-plugin-json';
+import string from 'rollup-plugin-string';
+
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+
+function pgl(plugins=[]) {
+  return [
+    resolve(),
+    json(),
+    string({
+      include: '**/*.svg'
+    }),
+    ...plugins,
+    commonjs()
+  ];
+}
+
+export default [
+  {
+    input: 'lib/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: pgl(),
+    external: [
+      'min-dash',
+      'min-dom'
+    ]
+  }
+];
