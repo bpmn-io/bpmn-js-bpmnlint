8351ac1c1d9560027c9f5ffbce2d9aee7e4ea407
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-20 10:54:29 +0200

    feat(example): persist linting state in url

--- example/src/app.js
+++ example/src/app.js
@@ -17,3 +17,43 @@ var modeler = new BpmnModeler({
 });
 
 modeler.importXML(diagramXML);
+
+modeler.on('linting.toggle', function(event) {
+
+  var active = event.active;
+
+  setUrlParam('linting', active);
+});
+
+
+function setUrlParam(name, value) {
+
+  var url = new URL(window.location.href);
+
+  if (value) {
+    url.searchParams.set(name, 1);
+  } else {
+    url.searchParams.delete(name);
+  }
+
+  window.history.replaceState({}, null, url.href);
+}
+
+function getUrlParam(name) {
+  var url = new URL(window.location.href);
+
+  return url.searchParams.has(name);
+}
+
+
+modeler.on('import.done', function() {
+  const active = getUrlParam('linting');
+
+  const linting = modeler.get('linting');
+
+  if (active) {
+    linting.activateLinting();
+  } else {
+    linting.deactivateLinting();
+  }
+});
