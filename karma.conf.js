fff183543612c5a80ac7ad1011abe1d19b43a1f8
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-25 11:19:43 +0200

    chore(project): add basic test
    Closes https://github.com/bpmn-io/bpmn-js-bpmnlint/issues/7
    
    :tada:

---
+++ karma.conf.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var path = require('path');
+
+var absoluteBasePath = path.resolve(__dirname);
+
+/* global process */
+
+// configures browsers to run test against
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
+var browsers =
+  (process.env.TEST_BROWSERS || 'ChromeHeadless')
+    .replace(/^\s+|\s+$/, '')
+    .split(/\s*,\s*/g)
+    .map(function(browser) {
+      if (browser === 'ChromeHeadless') {
+        process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+        // workaround https://github.com/GoogleChrome/puppeteer/issues/290
+        if (process.platform === 'linux') {
+          return 'ChromeHeadless_Linux';
+        }
+      }
+
+      return browser;
+    });
+
+var suite = 'test/suite.js';
+
+
+module.exports = function(karma) {
+  karma.set({
+
+    frameworks: [
+      'mocha',
+      'sinon-chai'
+    ],
+
+    files: [
+      suite
+    ],
+
+    preprocessors: {
+      [suite]: [ 'webpack' ]
+    },
+
+    reporters: [ 'progress' ],
+
+    browsers: browsers,
+
+    browserNoActivityTimeout: 30000,
+
+    customLaunchers: {
+      ChromeHeadless_Linux: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ],
+        debug: true
+      }
+    },
+
+    autoWatch: false,
+    singleRun: true,
+
+    webpack: {
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.bpmnlintrc$/,
+            use: 'bpmnlint-loader'
+          },
+          {
+            test: /\.(css|bpmn|svg)$/,
+            use: 'raw-loader'
+          }
+        ]
+      },
+      resolve: {
+        modules: [
+          'node_modules',
+          absoluteBasePath
+        ]
+      }
+    }
+  });
+};
