ff647469e317eff72f49a56dedb2e407b303d4b3
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-30 15:51:28 +0200

    feat(linting): allow lint configuration to be set manually
    * emit linting life-cycle events
    * expose #setLinterConfig method

--- test/spec/BpmnLintSpec.js
+++ test/spec/BpmnLintSpec.js
@@ -1,30 +1,150 @@
 import {
-  bootstrapModeler,
-  injectCSS,
-  inject
+  insertCSS
 } from 'bpmn-js/test/helper';
+
+import Modeler from 'bpmn-js/lib/Modeler';
 
 import LintModule from '../../lib';
 
 import bpmnlintrc from './.bpmnlintrc';
 
+insertCSS('bpmn-js-bpmnlint', require('assets/css/bpmn-js-bpmnlint.css'));
+
 
 describe('bpmn-js-bpmnlint', function() {
 
-  const bpmnDiagram = require('./diagram.bpmn');
+  it('should load specified config', function() {
 
-  beforeEach(bootstrapModeler(bpmnDiagram, {
-    additionalModules: [
-      LintModule
-    ],
-    linting: {
-      bpmnlint: bpmnlintrc
-    }
-  }));
+    // given
+    var modeler = new Modeler({
+      additionalModules: [
+        LintModule
+      ],
+      linting: {
+        bpmnlint: bpmnlintrc
+      }
+    });
 
+    // when
+    var linting = modeler.get('linting');
 
-  it('should work', inject(function(linting) {
+    // then
     expect(linting).to.exist;
-  }));
+  });
+
+
+  it('should start without config', function() {
+
+    // given
+    var modeler = new Modeler({
+      additionalModules: [
+        LintModule
+      ]
+    });
+
+    // when
+    var linting = modeler.get('linting');
+
+    // then
+    expect(linting).to.exist;
+  });
+
+
+  it('should not fail on broken config', function() {
+
+    // given
+    var modeler = new Modeler({
+      additionalModules: [
+        LintModule
+      ],
+      linting: {
+        bpmnlint: 'FOO BAR'
+      }
+    });
+
+    // when
+    var linting = modeler.get('linting');
+
+    // then
+    expect(linting).to.exist;
+  });
+
+
+  it('should lazy provide config', function() {
+
+    // given
+    var modeler = new Modeler({
+      additionalModules: [
+        LintModule
+      ]
+    });
+
+    var linting = modeler.get('linting');
+
+    // when
+    linting.setLinterConfig(bpmnlintrc);
+
+    // then
+    expect(linting.getLinterConfig()).to.eql(bpmnlintrc);
+  });
+
+
+  it('should throw when lazily providing broken config', function() {
+
+    // given
+    var modeler = new Modeler({
+      additionalModules: [
+        LintModule
+      ]
+    });
+
+    var linting = modeler.get('linting');
+
+    // then
+    expect(function() {
+      // when
+      linting.setLinterConfig('FOO');
+    }).to.throw('Expected linterConfig = { config, resolver }');
+  });
+
+
+  it('should re-lint on configuration change', function(done) {
+
+    // given
+    var modeler = new Modeler({
+      additionalModules: [
+        LintModule
+      ]
+    });
+
+    var diagram = require('./diagram.bpmn');
+
+    modeler.importXML(diagram, function(err) {
+
+      if (err) {
+        return done(err);
+      }
+
+      var linting = modeler.get('linting');
+
+      var eventBus = modeler.get('eventBus');
+
+      // when
+      linting.activateLinting();
+
+      eventBus.once('linting.completed', function(event) {
+
+        // then
+        var issues = event.issues;
+
+        expect(Object.keys(issues)).to.have.length(3);
+
+        done();
+      });
+
+      linting.setLinterConfig(bpmnlintrc);
+    });
+
+  });
 
 });
