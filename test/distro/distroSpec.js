d633dc9cc6ba8e73187da8520c9436835dc1a081
Nico Rehwaldt <git_nikku@nixis.de>
2020-12-14 11:02:42 +0100

    test: verify CJS and UMD exports

--- test/distro/distroSpec.js
+++ test/distro/distroSpec.js
@@ -1,9 +1,10 @@
 describe('distro', function() {
 
   it('should expose CJS bundle', function() {
-    const BpmnJSBpmnlint = require('../..');
+    const BpmnJSBpmnlint = require('../../dist/index.js');
 
     expect(BpmnJSBpmnlint).to.exist;
+    expect(BpmnJSBpmnlint.__init__).to.exist;
   });
 
 
@@ -11,6 +12,7 @@ describe('distro', function() {
     const BpmnJSBpmnlint = require('../../dist/bpmn-js-bpmnlint.umd.js');
 
     expect(BpmnJSBpmnlint).to.exist;
+    expect(BpmnJSBpmnlint.__init__).to.exist;
   });
 
 });
